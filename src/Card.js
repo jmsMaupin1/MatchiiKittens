52a5a01a092c1b3b6aaee6da22694e0395d6beae
Shannon McKinney <srmckinn@gmail.com>
2017-02-26 18:01:18 -0500

    Finish Initial App version

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Card extends Component {
+	constructor(props) {
+		super(props);
+		console.log(props)
+	}
+
 	render() {
+		var divStyle = {
+			backgroundImage: "url(" + this.props.card.image  + ")",
+			display: this.props.card.visible ? "inline-block" : "none"
+			// border:"1px solid black"
+		};
+
+		if(!this.props.card.visible) {
+			return null;
+		}
+
 		return (
-			<div className="kitten-card">
+			<div onClick={this.props.onClick.bind(this, this.props.index)} className="kitten-card" style={this.props.card.selected ? divStyle: null}>
 
 			</div>
 		);
